cee4ff96b115ad63ad60dbf1030119bb2a2398e7
Taras Kushnir <kushnirTV@gmail.com>
2018-01-21 23:16:27 +0200

    First iteration of sources

---
+++ temperature/arduino/sendtemp.ino
@@ -0,0 +1,60 @@
+#include <RCSwitch.h>
+
+RCSwitch tempSwitch = RCSwitch();
+
+#define HEADER 0x2D
+#define TEMP_STEP 0.0049438476f
+#define TEMP_MIN -30.f
+#define TEMP_MAX 50.f
+
+#define SENSOR_ID 2
+
+unsigned int getTemperatureCode(float temperature) {
+  float x = (temperature - TEMP_MIN)/TEMP_STEP;
+  unsigned int code = (unsigned int)x;
+  return code;
+}
+
+unsigned long createTemperatureMessage(byte id, float temperature) {
+  unsigned long result = 0;
+
+  // 6 bit header
+  result |= HEADER;
+
+  // 6 bit id
+  result = result << 6;
+  result |= (id & 0x3F);
+
+  // 14 bits - temperature code
+  const unsigned int tempCode = getTemperatureCode(temperature);
+  result = result << 14;
+  result |= tempCode & 0x3FFF;
+  
+  unsigned int checksum = tempCode;
+  checksum ^= id;
+  checksum ^= HEADER;
+  checksum ^= (tempCode >> 8);
+  
+  // 6 bits checksum
+  result = result << 6;
+  result |= (checksum & 0x3F);
+
+  return result;
+}
+  
+void setup() {
+  tempSwitch.enableTransmit(8);
+
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  unsigned long message = createTemperatureMessage(SENSOR_ID, 22.5f);
+  tempSwitch.send(message, 32);
+
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  
+  delay(5000);
+}
---
+++ temperature/rpi/tempserver.cpp
@@ -0,0 +1,85 @@
+#include "RCSwitch.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#define HEADER 0x2D
+#define TEMP_MIN -30.0f
+#define TEMP_STEP 0.0049438476f
+
+typedef unsigned int uint;
+
+RCSwitch tempSwitch;
+
+void handleReading(unsigned int value) {
+  bool success = false;
+  float temperature = 0.0f;
+  const uint originalValue = value;
+
+  do {
+    uint receivedChecksum = value & 0x3F;
+    value >>= 6;
+  
+    uint tempCode = value & 0x3FFF;
+    value >>= 14;
+
+    uint id = value & 0x3F;
+    value >>= 6;
+
+    uint header = value & 0x3F;
+  
+    if (header != HEADER) {
+      printf("Protocol violation: header mismatch. Received: %u\n", header);
+      break;
+    }
+
+    uint checksum = tempCode;
+    checksum ^= id;
+    checksum ^= header;
+    checksum ^= (tempCode >> 8);
+    checksum = checksum & 0x3F;
+
+    if (checksum != receivedChecksum) {
+      printf("Protocol violation: checksum mismatch");
+      break;
+    } else {
+      printf("Temp code: %u\n", tempCode);
+    }
+
+    temperature = TEMP_MIN + TEMP_STEP * (float)tempCode;
+
+    success = true;
+  } while (false);
+
+  if (success) {
+    printf("Received temperature: %.6f\n", temperature);
+  } else {
+    printf("Failed to process something: %u\n", originalValue);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  if (wiringPiSetup() < 0) {
+    printf("Failed to initialize wiringPi\n");
+    return 1;
+  } else {
+    printf("WiringPi is initialized\n");
+  }
+
+  const int PIN = 2;
+  tempSwitch = RCSwitch();
+  tempSwitch.enableReceive(PIN);
+  printf("Listening on pin %d...\n", PIN);
+    
+  while (1) {
+    if (tempSwitch.available()) {
+      int value = tempSwitch.getReceivedValue();
+      tempSwitch.resetAvailable();
+
+      handleReading((unsigned int)value);
+    }
+
+    fflush(stdout);
+  }
+
+  return 0;
+}
