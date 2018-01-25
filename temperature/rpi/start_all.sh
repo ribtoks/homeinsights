9ecf56399db4335f59bd7dc26b32c452606493a5
Taras Kushnir <kushnirTV@gmail.com>
2018-01-26 01:07:06 +0200

    Start tweak for webserver

--- temperature/rpi/start_all.sh
+++ temperature/rpi/start_all.sh
@@ -6,6 +6,9 @@ echo "Current PID: $$"
 ../tempserver/tempserver temps.db &
 echo "Tempserver PID: $!"
 
+echo "Waiting for tempserver to init DB..."
+sleep 3s
+
 ../webserver/webserver -db-path temps.db &
 echo "Webserver PID: $!"
 
