e6fa04400d5e19ee662e880abe3c8aac45e51bdd
Taras Kushnir <kushnirTV@gmail.com>
2018-07-03 08:42:23 +0300

    Revert "Cleanup"
    This reverts commit a407cabb4743cb98e616d6736e5c4e9526a6937f.

--- temperature/rpi/tempserver/tempserver.cpp
+++ temperature/rpi/tempserver/tempserver.cpp
@@ -116,6 +116,8 @@ int main(int argc, char *argv[]) {
         return DB_ERROR;
     }
 
+    clock_t cbegin = std::clock();
+    
     while (1) {
         if (tempSwitch.available()) {
             int value = tempSwitch.getReceivedValue();
